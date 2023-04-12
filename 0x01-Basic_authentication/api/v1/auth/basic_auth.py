6e191af30df431c2fe1e74babf58a558e521a58a
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-04-12 14:37:03 +0100

    added basicauth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Defining Basic Authentication class - BasicAuth
+"""
+
+import base64
+from auth import Auth
+from typing import TypeVar
+
+class BasicAuth(Auth):
+    """
+    Implement Basic Authorization protocol methods
+    """
+    pass
