ab2d2a8c196b1fcc4f6db07f9f3d3cb00c1b3192
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-04-12 16:13:15 +0100

    added user_object function

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,6 +7,8 @@ import base64
 from .auth import Auth
 from typing import TypeVar
 
+from models.user import User
+
 
 class BasicAuth(Auth):
     """
@@ -66,3 +68,24 @@ class BasicAuth(Auth):
             len(user_email) + 1:
             ]
         return (user_email, user_password)
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """
+        Return a User instance based on email and password
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            users = User.search({"email": user_email})
+            if not users or users == []:
+                return None
+            for u in users:
+                if u.is_valid_password(user_pwd):
+                    return u
+            return None
+        except Exception:
+            return None
