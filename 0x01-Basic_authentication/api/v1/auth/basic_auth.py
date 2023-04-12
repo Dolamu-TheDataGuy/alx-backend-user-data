e45210dc9d72e25f5f67d003c634980968545bd4
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-04-12 15:18:17 +0100

    added new method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,7 +13,8 @@ class BasicAuth(Auth):
     Implement Basic Authorization protocol methods
     """
     def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
+                                            authorization_header: str
+                                            ) -> str:
         """
         Extracts the Base64 part of the Authorization header for a Basic
         Authorization
@@ -26,3 +27,22 @@ class BasicAuth(Auth):
             return None
         token = authorization_header.split(" ")[-1]
         return token
+    
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """
+        Decode a Base64-encoded string
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_auth = base64_authorization_header.encode('utf-8') # code prone to error
+            decoded_auth = base64.b64decode(decoded_auth)
+        except Exception:  # code to run if there an error or exception
+            return None 
+        else:
+            return decoded_auth.decode('utf-8')  # run if code in try block is successful
